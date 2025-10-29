9e8868c8b859b3a4b69c748c4290bce471ae7a82
Mohammad Chowdhury <engrmkchow@email.com>
2025-10-28 21:33:49 -0600

    Reoshee header removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-screen w-full bg-yellow-50">
+    <main className="flex items-center justify-center h-screen w-full bg-red-50">
       <h1 className="text-3xl font-medium text-blue-700">
         Welcome to BuyerPanda
       </h1>
